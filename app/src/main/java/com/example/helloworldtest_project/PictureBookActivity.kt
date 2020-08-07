71c77d27f63b32a7f7bc2d8930798c1dab905461
Muthoni-Maker <kamauesthermuthoni@gmail.com>
2020-08-07 14:09:29 +0300

    my second push

---
+++ app/src/main/java/com/example/helloworldtest_project/PictureBookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helloworldtest_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PictureBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_book)
+    }
+}
